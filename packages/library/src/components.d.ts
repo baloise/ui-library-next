0d99584abfe4a3baa64f5cd26a7993d926816be8
hirsch <gery.hirschfeld@w3tec.ch>
2020-11-08 16:02:25 +0100

    feat(input): add autocomplete

--- packages/library/src/components.d.ts
+++ packages/library/src/components.d.ts
@@ -324,6 +324,10 @@ export namespace Components {
     }
     interface BalInput {
         /**
+          * The autocomplete attribute specifies whether or not an input field should have autocomplete enabled.
+         */
+        "autoComplete": boolean;
+        /**
           * If `true` the input gets a clickable cursor style
          */
         "clickable": boolean;
@@ -910,6 +914,10 @@ declare namespace LocalJSX {
         "turn"?: boolean;
     }
     interface BalInput {
+        /**
+          * The autocomplete attribute specifies whether or not an input field should have autocomplete enabled.
+         */
+        "autoComplete"?: boolean;
         /**
           * If `true` the input gets a clickable cursor style
          */
