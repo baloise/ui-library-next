f13fd98d8c2b3047b884c09d705b10f878a9401c
hirsch <gery.hirschfeld@w3tec.ch>
2020-11-13 10:38:01 +0100

    fix(): add utils and types

--- packages/library/src/index.ts
+++ packages/library/src/index.ts
@@ -1,3 +1,12 @@
 export { Components, JSX } from './components'
 
+/**
+ * Types
+ */
+export * from './components/bal-select-option/bal-select-option.type'
+export * from './components/bal-card-step/bal-card-step.type'
+
+/**
+ * Utils
+ */
 export * from './components/bal-select-option/bal-select-option.util'
